bce4d77157e77ea5e8bca9cef3b22b87c23e3be9
TitamSeptian <dwiputra4456@protonmail.com>
2022-03-25 10:22:35 +0700

    add some style

--- assets/js/room-tour.js
+++ assets/js/room-tour.js
@@ -20,3 +20,63 @@ divPannelum.addEventListener("click", function (event) {
     // }
     console.log(p360.getPitch(), p360.getYaw());
 });
+
+$(document).ready(function () {
+    // $(".panorama .info-destination .content button").click(function (event) {
+    //     $(".panorama .container .buttons").find(".button-info").css({
+    //         transform: "translateX(0%)",
+    //     });
+    // });
+
+    $(".info-destination .content")
+        .find("button")
+        .click(function (event) {
+            $(".info-destination").removeClass("active");
+        });
+
+    $(".panorama .container .buttons")
+        .find(".button-info")
+        .click(function (event) {
+            $(".info-destination").addClass("active");
+            $(this).css({
+                transform: "translateX(120%)",
+            });
+        });
+    $(".panorama .info-destination .content button").click(function (event) {
+        $(".panorama .container .buttons").find(".button-info").css({
+            transform: "translateX(0%)",
+        });
+    });
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $(".panorama .container .buttons").removeClass("in");
+            $(".panorama .container .buttons").removeClass("half");
+        } else {
+            if ($(".panorama .container .buttons").data("class") == "half") {
+                $(".panorama .container .buttons").addClass("half");
+            } else {
+                $(".panorama .container .buttons").addClass("in");
+            }
+        }
+    });
+    // arrow right
+    $(".panorama .container .buttons")
+        .find(".arrow-right")
+        .click(function (event) {
+            // alert("heo");
+            $(".panorama .container .buttons")
+                .removeClass("in")
+                .addClass("half")
+                .data("class", "half");
+        });
+    // arrow left
+    $(".panorama .container .buttons")
+        .find(".arrow-left")
+        .click(function (event) {
+            $(".panorama .container .buttons")
+                .removeClass("half")
+                .addClass("in")
+                .data("class", "in");
+        });
+});
