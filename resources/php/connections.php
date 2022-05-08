82482421e79cce66e6a98dce8228ac751631ad19
TitamSeptian <dwiputra4456@protonmail.com>
2022-05-09 02:58:46 +0700

    update connection

--- resources/php/connections.php
+++ resources/php/connections.php
@@ -1,11 +1,26 @@
 <?php
 function connectMySQL()
 {
+    $PORT = '3306'; // default 3306
+    $DATABASE_HOST = 'localhost:' . $PORT;
+    $DATABASE_USER = 'root';
+    $DATABASE_PASS = '';
+    $DATABASE_NAME = 'upi_dimensional_drift';
+    try {
+        return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
+    } catch (PDOException $exception) {
+        print_r($exception);
+        exit('Failed to connect to database!');
+    }
+}
+
+function connectMySQL2()
+{
     $PORT = '4000'; // default 3306
     $DATABASE_HOST = 'localhost:' . $PORT;
     $DATABASE_USER = 'root';
     $DATABASE_PASS = '123qwe123';
-    $DATABASE_NAME = 'db_pweb';
+    $DATABASE_NAME = 'upi_dimensional_drift';
     try {
         return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
     } catch (PDOException $exception) {
@@ -19,7 +34,7 @@ function mysqlCon()
     $host        = "localhost:4000";
     $username    = "root";
     $password    = "123qwe123";
-    $db            = "db_pweb";
+    $db          = "upi_dimensional_drift";
 
     $mysqli     = new mysqli($host, $username, $password, $db);
     if (mysqli_connect_errno()) {
